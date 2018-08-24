800286866c48e13426384c98dac50620388a44a3
BuiHai <haibuai3@gmail.com>
2018-08-24 12:43:33 +0700

    Finished homework

--- Week_2/homework1.py
+++ Week_2/homework1.py
@@ -1,9 +1,9 @@
 for i in range(1,101):
-    if i % 3 == 0:
-        print("fizz")
-    elif i % 5 ==0:
-        print("buzz")
-    elif(i % 3 ==0 and i % 5 == 0):
-        print("fizzbuzz")
+    if(i % 3 !=0 and i % 5 != 0):
+        print(i,end='')
     else :
-        print(i)
+        if i % 3 == 0:
+            print("fizz",end='')
+        if i % 5 ==0:
+            print("buzz",end='')
+    print(end='\n')
