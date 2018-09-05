19b7394d1a624e378a6981e975412b6e47abfec9
buingochai <haibuai3@gmail.com>
2018-09-05 12:22:10 +0700

    Week_3Fix

--- Week_3/homework10.py
+++ Week_3/homework10.py
@@ -1,3 +1,9 @@
 import numpy as np
 x = np.random.randint(0, 100, 20)
+y = np.random.uniform(0, 20)
+y
 print(x)
+print(y)
+index = np.argmin(np.abs(np.array(x)-y))
+print(" index of x where the value at that index is closest to y ",index)
+print(" value at that index is closest to yin x: ",x[index])
--- Week_3/homework8.py
+++ Week_3/homework8.py
@@ -1,3 +1,3 @@
 import numpy as np
 a = np.random.random((30))
-print(a.sum()/30)
+print(np.mean(a))
